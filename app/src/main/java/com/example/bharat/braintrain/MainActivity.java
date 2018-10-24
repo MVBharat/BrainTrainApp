9a037ca2738eebb589a4e22687054062c02ff306
MVBharat <manebharat85@gmail.com>
2018-10-24 12:25:25 +0530

    add result value and random value to button

--- app/src/main/java/com/example/bharat/braintrain/MainActivity.java
+++ app/src/main/java/com/example/bharat/braintrain/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
+    TextView sumTextView;
+    ArrayList<Integer> answer;
+    int locationOfCorrectAnswer;
     public void start(View view){
         startButton.setVisibility(View.INVISIBLE);
     }
@@ -17,7 +24,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startButton = (Button)findViewById(R.id.startButton);
+        sumTextView = (TextView)findViewById(R.id.sumTextView);
+        Random rand = new Random();
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
 
+        sumTextView.setText(Integer.toString(a) + Integer.toString(b));
 
+        locationOfCorrectAnswer=rand.nextInt(4);
+
+        for (int i = 0 ; i< 4; i++){
+            if(i==locationOfCorrectAnswer){
+                answer.add(a+b);
+            }else{
+                int inCorrectAnswer = rand.nextInt(41);
+                while(inCorrectAnswer == a + b){
+                    inCorrectAnswer = rand.nextInt(41);
+                }
+                answer.add(inCorrectAnswer);
+            }
+        }
     }
 }
